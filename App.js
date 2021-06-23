1996fc61460ae6d3d50a5fbab0ac0ce541e2542c
leocassiosilva <leocassiosilva1234@gmail.com>
2021-06-22 23:15:08 -0300

    header

--- App.js
+++ App.js
@@ -4,18 +4,65 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+          <View style={styles.box}>
+          </View>
+          <View style={styles.box}>
+          </View>
+          <View style={styles.box}>
+          </View>
+      </View>
+      <View style={styles.central}>
+          <Text>central</Text>
+		  </View>
+
+      <View style={styles.footer}>
+          <Text>footer</Text>
+		  </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:"column"
   },
+
+  header: {
+    flexDirection:'row',
+    flex: 0.2,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  box:{
+    width: 77,
+    height: 28,
+    top: 42,
+    backgroundColor: 'red',
+    borderRadius: 10,
+    
+		borderColor: 5, 
+		margin: 10,
+	},
+
+  text: {
+    color: 'black',
+    fontSize: 40,
+  },
+
+  central: {
+    flex: 0.6,
+    backgroundColor: 'red',
+  },
+
+  footer: {
+    flex: 0.2,
+    backgroundColor: 'blue',
+  } 
 });
