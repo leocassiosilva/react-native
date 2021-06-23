ffccd30494d88336d38e9d0bae38c47b074a3d6e
leocassiosilva <leocassiosilva1234@gmail.com>
2021-06-23 09:24:44 -0300

    conteudo

--- App.js
+++ App.js
@@ -25,8 +25,22 @@ export default function App() {
             <View style={styles.box_central_dentro1}><Text>2</Text></View>
             <View style={styles.box_central_dentro1}><Text>4</Text></View>
         </View>
-        <View>
-          <Text style={styles.box_central}>3</Text>
+        <View style={styles.box_central2}>
+            <View style={styles.box_central_dentro2}><Text>1</Text></View>
+            <View style={styles.box_central_dentro2}><Text>2</Text></View>
+            <View style={styles.box_central_dentro2}><Text>3</Text></View>
+            <View style={styles.box_central_dentro2}><Text>4</Text></View>
+            <View style={styles.box_central_dentro2}><Text>5</Text></View>
+            <View style={styles.box_central_dentro2}><Text>6</Text></View>
+            <View style={styles.box_central_dentro2}><Text>7</Text></View>
+            <View style={styles.box_central_dentro2}><Text>8</Text></View>
+            <View style={styles.box_central_dentro2}><Text>9</Text></View>
+            <View style={styles.box_central_dentro2}><Text>10</Text></View>
+            <View style={styles.box_central_dentro2}><Text>11</Text></View>
+            <View style={styles.box_central_dentro2}><Text>12</Text></View>
+            <View style={styles.box_central_dentro2}><Text>13</Text></View>
+            <View style={styles.box_central_dentro2}><Text>14</Text></View>
+            <View style={styles.box_central_dentro2}><Text>15</Text></View>
         </View>
 		  </View>
 
@@ -44,6 +58,7 @@ const styles = StyleSheet.create({
     flexDirection:"column"
   },
 
+  //Flex do centro
   central: {
     flex: 0.6,
     backgroundColor: '#E5E5E5',
@@ -67,11 +82,35 @@ const styles = StyleSheet.create({
 		margin: 10,
 	},
 
+
+  //central 2
+  box_central2:{
+    width: 230,
+    height: 124,
+    backgroundColor: '#B2B9FF',
+    justifyContent: 'space-evenly',
+    margin: 10,
+    flexDirection:"row", 
+    flexWrap:'wrap'
+	},
+
+  box_central_dentro2:{
+    width: 35,
+    height: 31,
+    backgroundColor: 'red',
+		margin: 5,
+	},
+
+
+
+
+    //central 1
   box_central1:{
     width: 230,
     height: 124,
     backgroundColor: '#B2B9FF',
-    justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
+    margin: 10,
 	},
   
   box_central_dentro1:{
@@ -79,10 +118,9 @@ const styles = StyleSheet.create({
     height: 31,
     backgroundColor: 'red',
 		margin: 10,
-    flexDirection:"row"
 	},
 
-  
+  //Fim do Centro 
 
 
   header: {
